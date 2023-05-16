cd1de2e132c163722e4acf709d773ae1ffefd668
Elia Ziliani <132611900+ZE-Group@users.noreply.github.com>
2023-05-16 07:20:42 +0200

    Update COVID19_ Project.sql

--- COVID19_ Project.sql
+++ COVID19_ Project.sql
@@ -87,3 +87,124 @@ GROUP BY covid_data.location, covid_data.population
 
 -- after the table has been created we can update 
 	--or alter the table to insert different information 
+
+--- getting the infection_percentage and the death_percentage from covid_data table
+
+select location, date, population, max(total_cases) as highest_cases,
+	ROUND((total_cases::numeric / population) * 100, 2) as infection_percentage,
+	round((total_deaths::numeric / total_cases)*100, 2) as death_percentage
+from covid_data
+group by location, population, total_cases, date, total_deaths
+limit 25;
+
+-- now gettin vaccination_percentage 
+
+	SELECT covid_data.location, covid_data.population, 
+		MAX(covid_vaccin.total_vaccinations::numeric) 
+			AS Highest_vaccination,
+	ROUND(MAX(covid_vaccin.total_vaccinations) / NULLIF(covid_data.population::numeric, 0) * 100, 5) 
+	   		AS vaccination_percentage
+FROM covid_data
+JOIN covid_vaccin ON covid_data.location = covid_vaccin.location 
+GROUP BY covid_data.location, covid_data.population
+ORDER BY covid_data.location
+LIMIT 20;
+
+-- visualizing the data together in a single table
+
+WITH covid_stats AS (
+    SELECT
+        location,
+        date,
+        population,
+        max(total_cases) AS highest_cases,
+        ROUND((total_cases::numeric / population) * 100, 2) AS infection_percentage,
+        ROUND((total_deaths::numeric / total_cases) * 100, 2) AS death_percentage
+    FROM
+        covid_data
+    GROUP BY
+        location,
+        date,
+        population,
+        total_cases,
+        total_deaths
+    LIMIT 25
+),
+vaccination_stats AS (
+    SELECT
+        covid_data.location,
+        covid_data.population,
+        MAX(covid_vaccin.total_vaccinations::numeric) AS highest_vaccination,
+        ROUND(MAX(covid_vaccin.total_vaccinations) / NULLIF(covid_data.population::numeric, 0) * 100, 5) 
+	AS vaccination_percentage
+    FROM
+        covid_data
+    JOIN covid_vaccin ON covid_data.location = covid_vaccin.location
+    GROUP BY
+        covid_data.location,
+        covid_data.population
+    ORDER BY
+        covid_data.location
+)
+SELECT
+    cs.location,
+    cs.date,
+    cs.population,
+    cs.highest_cases,
+    cs.infection_percentage,
+    cs.death_percentage,
+    vs.highest_vaccination,
+    vs.vaccination_percentage
+FROM
+    covid_stats cs
+JOIN vaccination_stats vs ON cs.location = vs.location;
+
+-- creating the last table with the total data and than move to Tableau
+
+CREATE TABLE Covid19_Stats AS (
+	WITH covid_stats AS (
+    SELECT
+        location,
+        date,
+        population,
+        max(total_cases) AS highest_cases,
+        ROUND((total_cases::numeric / population) * 100, 2) AS infection_percentage,
+        ROUND((total_deaths::numeric / total_cases) * 100, 2) AS death_percentage
+    FROM
+        covid_data
+    GROUP BY
+        location,
+        date,
+        population,
+        total_cases,
+        total_deaths
+),
+vaccination_stats AS (
+    SELECT
+        covid_data.location,
+        covid_data.population,
+        MAX(covid_vaccin.total_vaccinations::numeric) AS highest_vaccination,
+        ROUND(MAX(covid_vaccin.total_vaccinations) / NULLIF(covid_data.population::numeric, 0) * 100, 5)
+	AS vaccination_percentage
+    FROM
+        covid_data
+    JOIN covid_vaccin ON covid_data.location = covid_vaccin.location
+    GROUP BY
+        covid_data.location,
+        covid_data.population
+    ORDER BY
+        covid_data.location
+)
+SELECT
+    cs.location,
+    cs.date,
+    cs.population,
+    cs.highest_cases,
+    cs.infection_percentage,
+    cs.death_percentage,
+    vs.highest_vaccination,
+    vs.vaccination_percentage
+FROM
+    covid_stats cs
+JOIN vaccination_stats vs ON cs.location = vs.location
+);
